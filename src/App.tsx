f204a28486a02105453c9b7daad55d906f9b477b
vmc111 <vamship625@gmail.com>
2023-09-04 12:47:42 +0530

    Added Reviewchanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 
 import Login from "./components/Login";
 
